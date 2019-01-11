33d31e551e5ed08799acc91eb6ff49abbab6742b
João Paulo Pulga <joaopaulo1339@outlook.com>
2019-01-11 11:41:21 -0200

    Basic write, need a lot of work

--- source/app.d
+++ source/app.d
@@ -6,30 +6,36 @@ import std.experimental.xml;
 static struct MyHandler(NodeType)
 {
 	/**
-	 * We Can use
-	 * 	onDocument,
-	 * 	onElementStart,
-	 * 	onElementEnd, 
-	 * 	onElementEmpty, 
-	 * 	onText, 
-	 * 	onComment,
-	 * 	onProcessingInstruction,
-	 * 	onCDataSection
-	 */
+	* We Can use
+	*	onDocument,
+	*	onElementStart,
+	*	onElementEnd, 
+	*	onElementEmpty, 
+	*	onText, 
+	*	onComment,
+	*	onProcessingInstruction,
+	*	onCDataSection
+	*/
 	void onDocument(ref NodeType node) {
+		nodes = [];
 		writeln("Starting new document");
 	}
 
-    void onElementEnd(ref NodeType node) {
+	void onElementEnd(ref NodeType node) {
 		writeln("Node found: ", node.name);
-    }
+		nodes ~= node.name;
+	}
 }
 
 auto cursorErrorCallBack = (CursorError  err) {
 	// Do nothing
 };
 
+string[] nodes;
+
 void main() {
+	import std.path : baseName, dirSeparator, setExtension, stripExtension;
+
 	foreach (file; dirEntries("xml", "*.xml", SpanMode.shallow)) {
 		string contents = readText(file);
 
@@ -40,5 +46,8 @@ void main() {
 				.saxParser!MyHandler;
 
 		saxParser.processDocument;
+
+		File fl = File("output" ~ dirSeparator ~  file.baseName.setExtension("d"), "w");
+		fl.write("module xcb." ~ file.baseName.stripExtension() ~ ";");
 	}
 }
